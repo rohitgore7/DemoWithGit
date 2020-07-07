305c71dbe85e999497c265988e2724d2422106cc
Rohit <rohitgore516@gmail.com>
2020-07-07 15:29:30 +0530

    UserController added

---
+++ src/main/java/com/example/demo/SpringWithGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWithGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWithGitApplication.class, args);
+	}
+
+}
